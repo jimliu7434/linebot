432af411243de11f23945429f52e5fae92b947c2
JimLiu7434 <jimliu7434@gmail.com>
2017-04-01 14:08:48 +0800

    line bot finish v0.1

--- config/channel.js
+++ config/channel.js
@@ -1,5 +1,5 @@
 let config = {
-    channelid: "",
-    channelscrt: "",
-    mid: ""
+    id: "1508238448",
+    scrt: "a4488c430f3c2dfc9605961c3cafa557",
+    token: "U3a42daf2137730515723b4ed7585c0bd"
 }
---
+++ index.js
@@ -0,0 +1,23 @@
+const
+    express = require('express'),
+    linebot = require('linebot'),
+    channel = require('./config/channel.js'),
+    bot = linebot({
+        channelId: channel.id,
+        channelSecret: channel.scrt,
+        channelAccessToken: channel.token,
+    }),
+    app = express(),
+    linebotParser = bot.parser();
+
+
+bot.on('message', event => {
+    console.log(event);
+});
+
+app.post('/', linebotParser);
+
+var server = app.listen(process.env.PORT || 8080,
+    () => {
+        console.log(`App is now running on ${server.address().port}`)
+    });
