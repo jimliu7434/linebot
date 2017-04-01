69c1d31766223ebd42fb6fda2450c7e25497d659
JimLiu7434 <jimliu7434@gmail.com>
2017-04-02 01:34:38 +0800

    add debug log

--- src/game/answer.js
+++ src/game/answer.js
@@ -1,14 +1,16 @@
 var a = (maxleng) => {
+    console.log(`max length ${maxleng}`);
     let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
         ans = [];
 
     while (ans.length < maxleng){
         let numIndex = getRandom(0, arr.length - 1);
+        console.log(`chosed ${arr[numIndex]}`);
         if(!ans.indexOf(arr[numIndex])){
             ans.push(arr[numIndex]);
         }
     }
-
+    console.log(`ans ${ans.join('')}`);
     return ans.join('');
 };
 
