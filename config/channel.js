b4b7032575e1f8a9e75ffa634b6df3d47d473c70
JimLiu7434 <jimliu7434@gmail.com>
2017-04-01 14:17:07 +0800

    change token

--- config/channel.js
+++ config/channel.js
@@ -1,5 +1,5 @@
 let config = {
     id: "1508238448",
     scrt: "a4488c430f3c2dfc9605961c3cafa557",
-    token: "U3a42daf2137730515723b4ed7585c0bd"
+    token: "Hb2QicKaZE4UTFe/18yTuCkTIdkb+1/I3UsCw8OLoneTkg18VPPhJ6Y1OBN5AOGu+2qot/eJDxWjbaiebZml9OSSh2iv7CXknFlA8y8+d2vktd1yx6VQX4PFte86qdUR8f9gziuAko8MpJa2UCunFwdB04t89/1O/w1cDnyilFU="
 }
